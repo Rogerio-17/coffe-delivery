193c85981b690e6e095eaafcd7e9f3d1d6fa7fbe
ROGERIO JOSE <57935315+Rogerio-17@users.noreply.github.com>
2023-11-03 21:54:56 -0300

    finalizando página de home

--- src/components/CountCoffe/index.tsx
+++ src/components/CountCoffe/index.tsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import { CountCoffeContainer } from "./style";
 
-
 export function CountCoffe() {
+    const [coffeQtd, setCoffeQtd] = useState(0)
+
+    function some() {
+        setCoffeQtd(coffeQtd + 1)
+    }
+
+    function sub() {
+        if (coffeQtd > 0) {
+            setCoffeQtd(coffeQtd - 1)
+        }
+    }
+
     return (
         <CountCoffeContainer>
-        <button>-</button>
-        <input type="text" />
-        <button>+</button>
+        <button onClick={() => sub()}>-</button>
+        <input type="text" value={coffeQtd}/>
+        <button onClick={() => some()}>+</button>
         </CountCoffeContainer>
     )
 }
